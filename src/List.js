154057a37b1349781b5589abbb6b3426bdad7933
Sepehr <62986692+IIIphr@users.noreply.github.com>
2023-05-18 14:06:39 +0330

    Added code list page

---
+++ src/List.js
@@ -0,0 +1,11 @@
+import './List.css';
+
+function List() {
+  return (
+    <div className='list_root'>
+      <h1>لیست کدها</h1>
+    </div>
+  );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import List from './List';
 import reportWebVitals from './reportWebVitals';
 
 import {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/list",
+    element: <List />,
   },
 ]);
 
