f8a4d2b19e4af1d07cf65578d165ec508c60cb0f
Sepehr <62986692+IIIphr@users.noreply.github.com>
2023-05-18 15:09:04 +0330

    Used redux for user name, for now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const name = useSelector((state) => state.user.user_name);
+
   return (
     <div className='app_root'>
       <h1>به سامانه‌ی تبادل کد فراموشی خوش آمدید</h1>
+      <p>سلام {name}!</p>
       <a href='/login'>ورود به حساب</a>
       <a href='/list'>لیست کدها</a>
     </div>
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,19 @@
 import './Login.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { change_name } from './userSlice';
+import { useState } from 'react';
 
-function Login() {
+function Login(props) {
+  const dispatch = useDispatch();
+  const name = useSelector((state) => state.user.user_name);
+  const [input, setInput] = useState('');
+
   return (
     <div className='login_root'>
       <h1>ورود</h1>
+      <p>سلام {name}!</p>
+      <input type='text' id='name_input' value={input} onInput={e => setInput(e.target.value)}></input>
+      <button onClick={() => dispatch(change_name(input))}>تغییر نام</button>
       <a href='/signup'>ثبت نام</a>
       <a href='/'>صفحه‌ی اصلی</a>
     </div>
