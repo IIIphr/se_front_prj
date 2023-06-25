f1d07122bc9548542782d21c9e5f1a12a3b453ab
Sepehr <62986692+IIIphr@users.noreply.github.com>
2023-06-25 13:58:04 +0330

    Adding bootstrap to navbar...

--- src/Navbarr.js
+++ src/Navbarr.js
@@ -27,37 +27,15 @@ function Navbarr() {
     return (
         <Navbar sticky='top' expand="lg" className="bg-body-tertiary" data-bs-theme="dark" dir='rtl'>
             <Container>
-                <Navbar.Brand href='/'>صفحه‌ی اصلی</Navbar.Brand>
+                <Navbar.Brand><Link className='navbar_unlink' to='/'>سامانه‌ی تبادل کد فراموشی</Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Link className='navbar_link' to='/'>صفحه‌ی اصلی</Link>
                     {user_type == null ?
-                    <Link className='navbar_link' to='/login'>ورود</Link> :
-                    <Link className='navbar_link' to='/profile'>سلام {user_f_name}! - دانشگاه {user_uni_id}</Link>}
-                    {/* <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav> */}
+                    <Link className='navbar_unlink' to='/login'>ورود</Link> :
+                    <Link className='navbar_unlink' to='/profile'>سلام {user_f_name}! - دانشگاه {user_uni_id}</Link>}
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-        // <div className='navbar_root'>
-        //     <Link className='navbar_link' to='/'>صفحه‌ی اصلی</Link>
-        //     {user_type == null ? 
-        //     <Link className='navbar_link' to='/login'>ورود</Link> : 
-        //     <Link className='navbar_link' to='/profile'>سلام {user_f_name}! - دانشگاه {user_uni_id}</Link>}
-        // </div>
     );
 }
 
