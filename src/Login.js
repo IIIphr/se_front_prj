d4cb307a8fe0c88b7fe1b19d1f07de0e9c75a4f8
Sepehr <62986692+IIIphr@users.noreply.github.com>
2023-06-25 15:56:33 +0330

    Login page has style now

--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,9 @@ import { useState, useEffect, Fragment } from 'react';
 import { useCookies } from 'react-cookie';
 import { useNavigate, Link } from 'react-router-dom';
 import Navbarr from './Navbarr';
-import { RotatingLines } from 'react-loader-spinner';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Spinner from 'react-bootstrap/Spinner';
 
 function addMonths(date, months) {
   var d = date.getDate();
@@ -22,13 +24,13 @@ function Login() {
   const [id_input, set_id_input] = useState('');
   const [password_input, set_password_input] = useState('');
   const [is_loading, set_loading] = useState(0);
-  const [uni_input, set_uni_input] = useState(-1);
+  const [uni_input, set_uni_input] = useState(0);
   const navigate = useNavigate();
   const [cookies, setCookie] = useCookies(['user_f_name', 'user_type', 'user_uni_id', 'user_stu_id']);
 
   useEffect(() => {
-    if(user_type == null){
-      if(cookies.user_type !== "undefined" && cookies.user_type){
+    if (user_type == null) {
+      if (cookies.user_type !== "undefined" && cookies.user_type) {
         dispatch(change_f_name(cookies.user_f_name));
         dispatch(change_type(cookies.user_type));
         dispatch(change_uni_id(cookies.user_uni_id));
@@ -40,55 +42,55 @@ function Login() {
   return (
     <Fragment>
       <Navbarr />
-      <div className='login_root'>
-        <h1>ورود</h1>
-        <p className='rtl'>شماره‌ی دانشجویی</p>
-        <input type='number' id='id_input' value={id_input} onInput={e => set_id_input(e.target.value)}></input>
-        <p className='rtl'>دانشگاه</p>
-        <input type='number' id='uni_input' value={uni_input} onInput={e => set_uni_input(e.target.value)}></input>
-        <p className='rtl'>رمز</p>
-        <input type='text' id='password_input' value={password_input} onInput={e => set_password_input(e.target.value)}></input>
-        {is_loading == 0 ?
-        <button className='ui_btn' onClick={() => {
-          set_loading(1); 
-          fetch("http://localhost:4000/api/login",
-          {
-            method: "POST",
-            body: JSON.stringify({
-              "studentid": id_input,
-              "universityid": uni_input,
-              "password": password_input
-            })
-          })
-            .then(res => res.json())
-            .then((result) => {
-              if(result.stat == undefined){
-                dispatch(change_f_name(result.firstname));
-                dispatch(change_type("user"));
-                dispatch(change_uni_id(uni_input));
-                dispatch(change_credit(result.currentmoney));
-                dispatch(change_l_name(result.lastname));
-                dispatch(change_stu_id(id_input));
-                setCookie('user_f_name', result.firstname, { path: '/', expires: addMonths(new Date(), 1) });
-                setCookie('user_uni_id', uni_input, { path: '/', expires: addMonths(new Date(), 1) });
-                setCookie('user_stu_id', id_input, { path: '/', expires: addMonths(new Date(), 1) });
-                setCookie('user_type', "user", { path: '/', expires: addMonths(new Date(), 1) });
-                navigate('/profile');
-              }
-              else{
-                set_loading(0);
-              }
-            });
-        }}>ورود</button> :
-        <RotatingLines
-          strokeColor="black"
-          strokeWidth="5"
-          animationDuration="0.75"
-          width="50"
-          visible={true}
-        />}
-        <Link to='/signup'>ثبت نام</Link>
-      </div>
+      <Container className='d-flex flex-column justify-content-center align-items-center min-vh-75'>
+        <Card bg='dark' text='light' className='d-flex flex-column text-center p-3 m-3'>
+          <Card.Title>ورود</Card.Title>
+          <Card.Body className='d-flex flex-column justify-content-evenly align-items-center min-vh-50'>
+            شماره‌ی دانشجویی
+            <input type='number' id='id_input' value={id_input} onInput={e => set_id_input(e.target.value)}></input>
+            دانشگاه
+            <input type='number' id='uni_input' value={uni_input} onInput={e => set_uni_input(e.target.value)}></input>
+            رمز
+            <input type='text' id='password_input' value={password_input} onInput={e => set_password_input(e.target.value)}></input>
+            {is_loading == 0 ?
+              <button className='btn btn-outline-light text-center rtl w-100' onClick={() => {
+                set_loading(1);
+                fetch("http://localhost:4000/api/login",
+                  {
+                    method: "POST",
+                    body: JSON.stringify({
+                      "studentid": id_input,
+                      "universityid": uni_input,
+                      "password": password_input
+                    })
+                  })
+                  .then(res => res.json())
+                  .then((result) => {
+                    if (result.stat == undefined) {
+                      dispatch(change_f_name(result.firstname));
+                      dispatch(change_type("user"));
+                      dispatch(change_uni_id(uni_input));
+                      dispatch(change_credit(result.currentmoney));
+                      dispatch(change_l_name(result.lastname));
+                      dispatch(change_stu_id(id_input));
+                      setCookie('user_f_name', result.firstname, { path: '/', expires: addMonths(new Date(), 1) });
+                      setCookie('user_uni_id', uni_input, { path: '/', expires: addMonths(new Date(), 1) });
+                      setCookie('user_stu_id', id_input, { path: '/', expires: addMonths(new Date(), 1) });
+                      setCookie('user_type', "user", { path: '/', expires: addMonths(new Date(), 1) });
+                      navigate('/profile');
+                    }
+                    else {
+                      set_loading(0);
+                    }
+                  });
+              }}>ورود</button> :
+              <button className='btn btn-outline-light text-center rtl w-100' disabled>
+                <Spinner animation='border' size='sm'/>
+              </button>}
+            <Link className='btn btn-outline-light text-center rtl w-100' to='/signup'>ثبت نام</Link>
+          </Card.Body>
+        </Card>
+      </Container>
     </Fragment>
   );
 }
--- src/Success.js
+++ src/Success.js
@@ -3,7 +3,7 @@ import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import Navbarr from './Navbarr';
 import Card from 'react-bootstrap/Card';
-import { Container } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 
 function Success() {
   return (
@@ -11,9 +11,8 @@ function Success() {
       <Navbarr />
       <Container className='d-flex flex-column justify-content-center align-items-center min-vh-75'>
         <Card bg='dark' text='light' className='d-flex flex-column text-center p-3 m-3'>
-          {/* <Card.Title className='text-center rtl my-5'>پرداخت موفقیت آمیز بود!</Card.Title> */}
           <Card.Body>
-          <h3 className='text-center rtl mb-5'>پرداخت موفقیت آمیز بود!</h3>
+            <h3 className='text-center rtl mb-5'>پرداخت موفقیت آمیز بود!</h3>
             <Link className='btn btn-outline-light text-center rtl' to='/'>بازگشت به صفحه‌ی اصلی</Link>
           </Card.Body>
         </Card>
