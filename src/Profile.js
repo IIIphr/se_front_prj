5e6c66a22828a294246fe1afee9bd262c4638b35
Sepehr <62986692+IIIphr@users.noreply.github.com>
2023-06-26 07:47:06 +0330

    Testing user history api

--- src/Profile.js
+++ src/Profile.js
@@ -17,6 +17,8 @@ function Profile() {
   const name = useSelector((state) => state.user.user_f_name);
   const l_name = useSelector((state) => state.user.user_l_name);
   const user_type = useSelector((state) => state.user.user_type);
+  const stu_id = useSelector((state) => state.user.user_stu_id);
+  const uni_id = useSelector((state) => state.user.user_uni_id);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [validated_f, set_validated_f] = useState(false);
@@ -26,6 +28,7 @@ function Profile() {
   const [cookies] = useCookies(['user_type']);
   const [is_loading_f, set_loading_f] = useState(0);
   const [is_loading_l, set_loading_l] = useState(0);
+  const [is_loading_t, set_loading_t] = useState(0);
   const [show_tf, set_show_tf] = useState(false);
   const [show_tl, set_show_tl] = useState(false);
 
@@ -41,18 +44,18 @@ function Profile() {
     <Fragment>
       <Navbarr />
       <ToastContainer className='p-3' position='bottom-center' style={{ zIndex: 5 }}>
-      <Toast onClose={() => set_show_tf(false)} show={show_tf} delay={3000} autohide>
-        <Toast.Header>
-          <strong className="me-auto">تغییر</strong>
-        </Toast.Header>
-        <Toast.Body>نام شما تغییر کرد</Toast.Body>
-      </Toast>
-      <Toast onClose={() => set_show_tl(false)} show={show_tl} delay={3000} autohide>
-        <Toast.Header>
-          <strong className="me-auto">تغییر</strong>
-        </Toast.Header>
-        <Toast.Body>نام خانوادگی شما تغییر کرد</Toast.Body>
-      </Toast>
+        <Toast onClose={() => set_show_tf(false)} show={show_tf} delay={3000} autohide>
+          <Toast.Header>
+            <strong className="me-auto">تغییر</strong>
+          </Toast.Header>
+          <Toast.Body>نام شما تغییر کرد</Toast.Body>
+        </Toast>
+        <Toast onClose={() => set_show_tl(false)} show={show_tl} delay={3000} autohide>
+          <Toast.Header>
+            <strong className="me-auto">تغییر</strong>
+          </Toast.Header>
+          <Toast.Body>نام خانوادگی شما تغییر کرد</Toast.Body>
+        </Toast>
       </ToastContainer>
       <Container className='d-flex flex-column justify-content-center align-items-center min-vh-75'>
         <Card bg='dark' text='light' className='d-flex flex-column text-center p-3 m-3'>
@@ -124,6 +127,43 @@ function Profile() {
                   <Spinner animation='border' size='sm' />
                 </button>}
             </Form>
+
+            {is_loading_t == 0 ?
+              <button className='btn btn-outline-light text-center rtl' onClick={(e) => {
+                set_loading_t(1);
+                fetch("http://localhost:4000/api/history",
+                  {
+                    method: "POST",
+                    body: JSON.stringify({
+                      "studentid": stu_id,
+                      "universityid": uni_id
+                    })
+                  })
+                  .then(res => res.json())
+                  .then((result) => {
+                    console.log(result);
+                    set_loading_t(0);
+                    // if (result.stat == undefined) {
+                    //   dispatch(change_f_name(result.firstname));
+                    //   dispatch(change_type("user"));
+                    //   dispatch(change_uni_id(uni_input));
+                    //   dispatch(change_credit(result.currentmoney));
+                    //   dispatch(change_l_name(result.lastname));
+                    //   dispatch(change_stu_id(id_input));
+                    //   setCookie('user_f_name', result.firstname, { path: '/', expires: addMonths(new Date(), 1) });
+                    //   setCookie('user_uni_id', uni_input, { path: '/', expires: addMonths(new Date(), 1) });
+                    //   setCookie('user_stu_id', id_input, { path: '/', expires: addMonths(new Date(), 1) });
+                    //   setCookie('user_type', "user", { path: '/', expires: addMonths(new Date(), 1) });
+                    //   navigate('/profile');
+                    // }
+                    // else {
+                    //   set_loading_t(0);
+                    // }
+                  });
+              }}>history</button> :
+              <button className='btn btn-outline-light text-center rtl w-30' disabled>
+                <Spinner animation='border' size='sm' />
+              </button>}
           </Card.Body>
         </Card>
       </Container>
