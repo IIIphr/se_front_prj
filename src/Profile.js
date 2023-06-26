ef1d395480f269393917a0990f4f7f0a4a9632ae
Sepehr <62986692+IIIphr@users.noreply.github.com>
2023-06-26 15:32:39 +0330

    fixed update money

--- src/Profile.js
+++ src/Profile.js
@@ -10,8 +10,10 @@ import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Spinner from 'react-bootstrap/Spinner';
 import Toast from 'react-bootstrap/Toast';
-import { change_f_name, change_l_name, change_password } from './userSlice';
+import { change_credit, change_f_name, change_l_name, change_password } from './userSlice';
 import { ToastContainer } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 function addMonths(date, months) {
   var d = date.getDate();
@@ -27,7 +29,10 @@ function Profile() {
   const l_name = useSelector((state) => state.user.user_l_name);
   const user_type = useSelector((state) => state.user.user_type);
   const user_id = useSelector((state) => state.user.user_id);
+  const user_stu_id = useSelector((state) => state.user.user_stu_id);
+  const user_uni_id = useSelector((state) => state.user.user_uni_id);
   const user_password = useSelector((state) => state.user.user_password);
+  const user_credit = useSelector((state) => state.user.user_credit);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [validated_f, set_validated_f] = useState(false);
@@ -43,6 +48,35 @@ function Profile() {
   const [show_tf, set_show_tf] = useState(false);
   const [show_tl, set_show_tl] = useState(false);
   const [show_tp, set_show_tp] = useState(false);
+  const [show_tm, set_show_tm] = useState(false);
+  const [popup, set_popup] = useState(false);
+  const [money, set_money] = useState(0);
+
+  const handle_money = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:4000/api/update-money",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          "user": {
+            "_id": null,
+            "studentid": user_stu_id,
+            "firstname": null,
+            "lastname": null,
+            "universityid": user_uni_id,
+            "password": user_password,
+            "currentmoney": null,
+            "coupons": []
+          },
+          "money": Number(money)
+        })
+      })
+      .then(() => {
+        dispatch(change_credit(user_credit + parseInt(money)));
+        set_popup(false);
+        set_show_tm(true);
+      });
+  };
 
   useEffect(() => {
     if (user_type == null) {
@@ -55,6 +89,37 @@ function Profile() {
   return (
     <Fragment>
       <Navbarr />
+      <Modal
+        show={popup}
+        onHide={() => { set_popup(false) }}
+        backdrop="static"
+        keyboard={false}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>افزایش موجودی</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handle_money}>
+            <Form.Group>
+              <FloatingLabel dir='rtl' className='custom-class' label="مبلغ به ریال">
+                <Form.Control
+                  dir='ltr'
+                  onChange={e => set_money(e.target.value)}
+                  defaultValue={money}
+                  type='number'
+                  placeholder="مبلغ به ریال" />
+              </FloatingLabel>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => { set_popup(false) }}>
+            بستن
+          </Button>
+          <Button variant="primary" onClick={handle_money}>پرداخت</Button>
+        </Modal.Footer>
+      </Modal>
       <ToastContainer className='p-3' position='bottom-center' style={{ zIndex: 5 }}>
         <Toast onClose={() => set_show_tf(false)} show={show_tf} delay={3000} autohide>
           <Toast.Header>
@@ -73,6 +138,12 @@ function Profile() {
             <strong className="me-auto">تغییر</strong>
           </Toast.Header>
           <Toast.Body>رمز شما تغییر کرد</Toast.Body>
+        </Toast>
+        <Toast onClose={() => set_show_tm(false)} show={show_tm} delay={3000} autohide>
+          <Toast.Header>
+            <strong className="me-auto">تغییر</strong>
+          </Toast.Header>
+          <Toast.Body>موجودی شما تغییر کرد</Toast.Body>
         </Toast>
       </ToastContainer>
       <Container className='d-flex flex-column justify-content-center align-items-center min-vh-75'>
@@ -204,6 +275,9 @@ function Profile() {
                 </button>}
             </Form>
             <Link className='btn btn-outline-light text-center rtl' to='/history'>تاریخچه‌ی خرید</Link>
+            <button className='btn btn-outline-light text-center rtl' onClick={() => {
+              set_popup(true);
+            }}>افزایش موجودی</button>
           </Card.Body>
         </Card>
       </Container>
