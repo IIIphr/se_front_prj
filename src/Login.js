cbbfa7253631f1df28738308f9146ea668dc136c
Sepehr <62986692+IIIphr@users.noreply.github.com>
2023-06-26 19:45:07 +0330

    Added university prpoerly

--- src/Login.js
+++ src/Login.js
@@ -28,8 +28,10 @@ function Login() {
   const [focused, set_focused] = useState(false);
   const [password_input, set_password_input] = useState('');
   const [is_loading, set_loading] = useState(0);
+  const [is_loading_page, set_loading_page] = useState(1);
   const [uni_input, set_uni_input] = useState(0);
   const [validated, set_validated] = useState(false);
+  const [data, set_data] = useState([]);
   const navigate = useNavigate();
   const [cookies, setCookie] = useCookies(['user_id', 'user_f_name', 'user_type', 'user_uni_id', 'user_stu_id', 'user_password', 'user_l_name', 'user_credit']);
 
@@ -47,6 +49,16 @@ function Login() {
         navigate('/profile');
       }
     }
+
+    fetch("http://localhost:4000/api/get-universities",
+      {
+        method: "POST"
+      })
+      .then(res => res.json())
+      .then((result) => {
+        set_loading_page(0);
+        set_data(result);
+      });
   }, [user_type, cookies.user_type]);
 
   return (
@@ -56,94 +68,99 @@ function Login() {
         <Card bg='dark' text='light' className='min-vw-90 d-flex flex-column text-center p-3 m-3'>
           <Card.Body className='d-flex flex-column justify-content-evenly align-items-center min-vh-75 min-vw-75'>
             <h3>ورود</h3>
-            <Form noValidate validated={validated} className='d-flex flex-column justify-content-evenly align-items-center min-vh-50 text-dark'>
-              <Form.Group>
-                <FloatingLabel dir='rtl' className='custom-class' label="شماره‌ی دانشجویی‌">
-                  <Form.Control
-                    required
-                    ref={r => { if (r && !focused) { ReactDOM.findDOMNode(r).focus(); set_focused(true); } }}
-                    dir='ltr'
-                    onChange={e => set_id_input(e.target.value)}
-                    type="text"
-                    placeholder="شماره‌ی دانشجویی" />
-                </FloatingLabel>
-              </Form.Group>
+            {is_loading_page == 1 ?
+              <Spinner /> :
+              <Form noValidate validated={validated} className='d-flex flex-column justify-content-evenly align-items-center min-vh-50 text-dark'>
+                <Form.Group>
+                  <FloatingLabel dir='rtl' className='custom-class' label="شماره‌ی دانشجویی‌">
+                    <Form.Control
+                      required
+                      ref={r => { if (r && !focused) { ReactDOM.findDOMNode(r).focus(); set_focused(true); } }}
+                      dir='ltr'
+                      onChange={e => set_id_input(e.target.value)}
+                      type="text"
+                      placeholder="شماره‌ی دانشجویی" />
+                  </FloatingLabel>
+                </Form.Group>
 
-              <Form.Group>
-                <FloatingLabel dir='rtl' className='custom-class' label="دانشگاه">
-                  <Form.Control
-                    required
-                    dir='ltr'
-                    onChange={e => set_uni_input(e.target.value)}
-                    type="text"
-                    placeholder="دانشگاه" />
-                </FloatingLabel>
-              </Form.Group>
+                <Form.Group className='w-100'>
+                  <FloatingLabel dir='rtl' className='custom-class' label="دانشگاه">
+                    <Form.Select
+                      required
+                      dir='ltr'
+                      onChange={e => set_uni_input(e.target.value)}
+                      defaultValue="دانشگاه">
+                        {(data || []).map((record, index) => {
+                          return <option key={index}>{record.universityid}</option>
+                        })}
+                      </Form.Select>
+                  </FloatingLabel>
+                </Form.Group>
 
-              <Form.Group>
-                <FloatingLabel dir='rtl' className='custom-class' label="رمز">
-                  <Form.Control
-                    required
-                    dir='ltr'
-                    onChange={e => set_password_input(e.target.value)}
-                    type="password"
-                    placeholder="رمز" />
-                </FloatingLabel>
-              </Form.Group>
+                <Form.Group>
+                  <FloatingLabel dir='rtl' className='custom-class' label="رمز">
+                    <Form.Control
+                      required
+                      dir='ltr'
+                      onChange={e => set_password_input(e.target.value)}
+                      type="password"
+                      placeholder="رمز" />
+                  </FloatingLabel>
+                </Form.Group>
 
-              {is_loading == 0 ?
-                <button className='btn btn-outline-light text-center rtl w-100' onClick={(e) => {
-                  e.preventDefault();
-                  const form = e.currentTarget;
-                  if (form.checkValidity() === false) {
+                {is_loading == 0 ?
+                  <button className='btn btn-outline-light text-center rtl w-100' onClick={(e) => {
                     e.preventDefault();
-                    e.stopPropagation();
-                  }
-                  else {
-                    set_validated(true);
-                    set_loading(1);
-                    fetch("http://localhost:4000/api/login",
-                      {
-                        method: "POST",
-                        body: JSON.stringify({
-                          "studentid": id_input,
-                          "universityid": uni_input,
-                          "password": password_input
+                    const form = e.currentTarget;
+                    if (form.checkValidity() === false) {
+                      e.preventDefault();
+                      e.stopPropagation();
+                    }
+                    else {
+                      set_validated(true);
+                      set_loading(1);
+                      fetch("http://localhost:4000/api/login",
+                        {
+                          method: "POST",
+                          body: JSON.stringify({
+                            "studentid": id_input,
+                            "universityid": uni_input,
+                            "password": password_input
+                          })
                         })
-                      })
-                      .then(res => res.json())
-                      .then((result) => {
-                        if (result.stat == undefined) {
-                          dispatch(change_f_name(result.firstname));
-                          dispatch(change_type("user"));
-                          dispatch(change_uni_id(uni_input));
-                          dispatch(change_credit(result.currentmoney));
-                          dispatch(change_l_name(result.lastname));
-                          dispatch(change_password(password_input));
-                          dispatch(change_stu_id(id_input));
-                          dispatch(change_id(result._id));
-                          setCookie('user_f_name', result.firstname, { path: '/', expires: addMonths(new Date(), 1) });
-                          setCookie('user_l_name', result.lastname, { path: '/', expires: addMonths(new Date(), 1) });
-                          setCookie('user_uni_id', uni_input, { path: '/', expires: addMonths(new Date(), 1) });
-                          setCookie('user_stu_id', id_input, { path: '/', expires: addMonths(new Date(), 1) });
-                          setCookie('user_password', password_input, { path: '/', expires: addMonths(new Date(), 1) });
-                          setCookie('user_type', "user", { path: '/', expires: addMonths(new Date(), 1) });
-                          setCookie('user_id', result._id, { path: '/', expires: addMonths(new Date(), 1) });
-                          setCookie('user_credit', result.currentmoney, { path: '/', expires: addMonths(new Date(), 1) });
-                          navigate('/profile');
-                        }
-                        else {
-                          set_loading(0);
-                        }
-                      });
-                  }
-                }}>ورود</button> :
-                <button className='btn btn-outline-light text-center rtl w-100' disabled>
-                  <Spinner animation='border' size='sm' />
-                </button>}
-
-              <Link className='btn btn-outline-light text-center rtl w-100' to='/signup'>ثبت نام</Link>
-            </Form>
+                        .then(res => res.json())
+                        .then((result) => {
+                          if (result.stat == undefined) {
+                            dispatch(change_f_name(result.firstname));
+                            dispatch(change_type("user"));
+                            dispatch(change_uni_id(uni_input));
+                            dispatch(change_credit(result.currentmoney));
+                            dispatch(change_l_name(result.lastname));
+                            dispatch(change_password(password_input));
+                            dispatch(change_stu_id(id_input));
+                            dispatch(change_id(result._id));
+                            setCookie('user_f_name', result.firstname, { path: '/', expires: addMonths(new Date(), 1) });
+                            setCookie('user_l_name', result.lastname, { path: '/', expires: addMonths(new Date(), 1) });
+                            setCookie('user_uni_id', uni_input, { path: '/', expires: addMonths(new Date(), 1) });
+                            setCookie('user_stu_id', id_input, { path: '/', expires: addMonths(new Date(), 1) });
+                            setCookie('user_password', password_input, { path: '/', expires: addMonths(new Date(), 1) });
+                            setCookie('user_type', "user", { path: '/', expires: addMonths(new Date(), 1) });
+                            setCookie('user_id', result._id, { path: '/', expires: addMonths(new Date(), 1) });
+                            setCookie('user_credit', result.currentmoney, { path: '/', expires: addMonths(new Date(), 1) });
+                            navigate('/profile');
+                          }
+                          else {
+                            set_loading(0);
+                          }
+                        });
+                    }
+                  }}>ورود</button> :
+                  <button className='btn btn-outline-light text-center rtl w-100' disabled>
+                    <Spinner animation='border' size='sm' />
+                  </button>}
+                <Link className='btn btn-outline-light text-center rtl w-100' to='/signup'>ثبت نام</Link>
+              </Form>
+            }
           </Card.Body>
         </Card>
       </Container>
--- src/Signup.js
+++ src/Signup.js
@@ -28,11 +28,13 @@ function Signup() {
   const [name_input, set_name_input] = useState('');
   const [last_name_input, set_last_name_input] = useState('');
   const [password_input, set_password_input] = useState('');
+  const [is_loading_page, set_loading_page] = useState(1);
   const [uni_input, set_uni_input] = useState(-1);
   const [id_input, set_id_input] = useState(-1);
   const [validated, set_validated] = useState(false);
   const [focused, set_focused] = useState(false);
   const navigate = useNavigate();
+  const [data, set_data] = useState([]);
   const [cookies, setCookie] = useCookies(['user_id', 'user_f_name', 'user_type', 'user_uni_id', 'user_stu_id', 'user_password', 'user_l_name', 'user_credit']);
 
   useEffect(() => {
@@ -49,6 +51,16 @@ function Signup() {
         navigate('/profile');
       }
     }
+
+    fetch("http://localhost:4000/api/get-universities",
+      {
+        method: "POST"
+      })
+      .then(res => res.json())
+      .then((result) => {
+        set_loading_page(0);
+        set_data(result);
+      });
   }, []);
 
   return (
@@ -58,113 +70,122 @@ function Signup() {
         <Card bg='dark' text='light' className='min-vw-90 d-flex flex-column text-center p-3 m-3'>
           <Card.Body className='d-flex flex-column justify-content-evenly align-items-center min-vh-75 min-vw-75'>
             <h3>ثبت نام</h3>
-            <Form noValidate validated={validated} className='d-flex flex-column justify-content-evenly align-items-center min-vh-65 text-dark'>
-              <Form.Group>
-                <FloatingLabel dir='rtl' className='custom-class' label="نام">
-                  <Form.Control
-                    required
-                    ref={r => { if (r && !focused) { ReactDOM.findDOMNode(r).focus(); set_focused(true); } }}
-                    dir='rtl'
-                    onChange={e => set_name_input(e.target.value)}
-                    type="text"
-                    placeholder="نام" />
-                </FloatingLabel>
-              </Form.Group>
+            {is_loading_page == 1 ?
+              <Spinner /> :
+              <Form noValidate validated={validated} className='d-flex flex-column justify-content-evenly align-items-center min-vh-65 text-dark'>
+                <Form.Group>
+                  <FloatingLabel dir='rtl' className='custom-class' label="نام">
+                    <Form.Control
+                      required
+                      ref={r => { if (r && !focused) { ReactDOM.findDOMNode(r).focus(); set_focused(true); } }}
+                      dir='rtl'
+                      onChange={e => set_name_input(e.target.value)}
+                      type="text"
+                      placeholder="نام" />
+                  </FloatingLabel>
+                </Form.Group>
 
-              <Form.Group>
-                <FloatingLabel dir='rtl' className='custom-class' label="نام خانوادگی">
-                  <Form.Control
-                    required
-                    dir='rtl'
-                    onChange={e => set_last_name_input(e.target.value)}
-                    type="text"
-                    placeholder="نام خانوادگی" />
-                </FloatingLabel>
-              </Form.Group>
+                <Form.Group>
+                  <FloatingLabel dir='rtl' className='custom-class' label="نام خانوادگی">
+                    <Form.Control
+                      required
+                      dir='rtl'
+                      onChange={e => set_last_name_input(e.target.value)}
+                      type="text"
+                      placeholder="نام خانوادگی" />
+                  </FloatingLabel>
+                </Form.Group>
 
-              <Form.Group>
-                <FloatingLabel dir='rtl' className='custom-class' label="شماره‌ی دانشجویی‌">
-                  <Form.Control
-                    required
-                    dir='ltr'
-                    onChange={e => set_id_input(e.target.value)}
-                    type="text"
-                    placeholder="شماره‌ی دانشجویی" />
-                </FloatingLabel>
-              </Form.Group>
+                <Form.Group>
+                  <FloatingLabel dir='rtl' className='custom-class' label="شماره‌ی دانشجویی‌">
+                    <Form.Control
+                      required
+                      dir='ltr'
+                      onChange={e => set_id_input(e.target.value)}
+                      type="text"
+                      placeholder="شماره‌ی دانشجویی" />
+                  </FloatingLabel>
+                </Form.Group>
 
-              <Form.Group>
-                <FloatingLabel dir='rtl' className='custom-class' label="دانشگاه">
-                  <Form.Control
-                    required
-                    dir='ltr'
-                    onChange={e => set_uni_input(e.target.value)}
-                    type="text"
-                    placeholder="دانشگاه" />
-                </FloatingLabel>
-              </Form.Group>
+                <Form.Group className='w-100'>
+                  <FloatingLabel dir='rtl' className='custom-class' label="دانشگاه">
+                    <Form.Select
+                      required
+                      dir='ltr'
+                      onChange={e => set_uni_input(e.target.value)}
+                      defaultValue="دانشگاه">
+                        {(data || []).map((record, index) => {
+                          return <option key={index}>{record.universityid}</option>
+                        })}
+                      </Form.Select>
+                  </FloatingLabel>
+                </Form.Group>
 
-              <Form.Group>
-                <FloatingLabel dir='rtl' className='custom-class' label="رمز">
-                  <Form.Control
-                    required
-                    dir='ltr'
-                    onChange={e => set_password_input(e.target.value)}
-                    type="password"
-                    placeholder="رمز" />
-                </FloatingLabel>
-              </Form.Group>
+                <Form.Group>
+                  <FloatingLabel dir='rtl' className='custom-class' label="رمز">
+                    <Form.Control
+                      required
+                      dir='ltr'
+                      onChange={e => set_password_input(e.target.value)}
+                      type="password"
+                      placeholder="رمز" />
+                  </FloatingLabel>
+                </Form.Group>
 
-              {is_loading == 0 ?
-                <button className='btn btn-outline-light text-center rtl w-100' onClick={(e) => {
-                  e.preventDefault();
-                  const form = e.currentTarget;
-                  if (form.checkValidity() === false) {
+                {is_loading == 0 ?
+                  <button className='btn btn-outline-light text-center rtl w-100' onClick={(e) => {
                     e.preventDefault();
-                    e.stopPropagation();
-                  }
-                  else {
-                    set_validated(true);
-                    set_loading(1);
-                    fetch("http://localhost:4000/api/signup",
-                      {
-                        method: "POST",
-                        body: JSON.stringify({
-                          "studentid": id_input,
-                          "firstname": name_input,
-                          "lastname": last_name_input,
-                          "universityid": uni_input,
-                          "password": password_input,
-                          "currentmoney": 0
+                    const form = e.currentTarget;
+                    if (form.checkValidity() === false) {
+                      e.preventDefault();
+                      e.stopPropagation();
+                    }
+                    else {
+                      set_validated(true);
+                      set_loading(1);
+                      fetch("http://localhost:4000/api/signup",
+                        {
+                          method: "POST",
+                          body: JSON.stringify({
+                            "studentid": id_input,
+                            "firstname": name_input,
+                            "lastname": last_name_input,
+                            "universityid": uni_input,
+                            "password": password_input,
+                            "currentmoney": 0
+                          })
                         })
-                      })
-                      .then(res => res.json())
-                      .then((result) => {
-                        dispatch(change_f_name(name_input));
-                        dispatch(change_type("user"));
-                        dispatch(change_uni_id(uni_input));
-                        dispatch(change_credit(0));
-                        dispatch(change_l_name(last_name_input));
-                        dispatch(change_password(password_input));
-                        dispatch(change_stu_id(id_input));
-                        dispatch(change_id(result._id));
-                        setCookie('user_f_name', name_input, { path: '/', expires: addMonths(new Date(), 1) });
-                        setCookie('user_l_name', last_name_input, { path: '/', expires: addMonths(new Date(), 1) });
-                        setCookie('user_uni_id', uni_input, { path: '/', expires: addMonths(new Date(), 1) });
-                        setCookie('user_stu_id', id_input, { path: '/', expires: addMonths(new Date(), 1) });
-                        setCookie('user_password', password_input, { path: '/', expires: addMonths(new Date(), 1) });
-                        setCookie('user_type', "user", { path: '/', expires: addMonths(new Date(), 1) });
-                        setCookie('user_id', result._id, { path: '/', expires: addMonths(new Date(), 1) });
-                        setCookie('user_credit', 0, { path: '/', expires: addMonths(new Date(), 1) });
-                      });
-                  }
-                }}>ثبت نام</button> :
-                <button className='btn btn-outline-light text-center rtl w-100' disabled>
-                  <Spinner animation='border' size='sm' />
-                </button>}
+                        .then(() => {
+                          navigate('/login');
+                        })
+                        // .then(res => res.json())
+                        // .then((result) => {
+                        //   dispatch(change_f_name(name_input));
+                        //   dispatch(change_type("user"));
+                        //   dispatch(change_uni_id(uni_input));
+                        //   dispatch(change_credit(0));
+                        //   dispatch(change_l_name(last_name_input));
+                        //   dispatch(change_password(password_input));
+                        //   dispatch(change_stu_id(id_input));
+                        //   dispatch(change_id(result._id));
+                        //   setCookie('user_f_name', name_input, { path: '/', expires: addMonths(new Date(), 1) });
+                        //   setCookie('user_l_name', last_name_input, { path: '/', expires: addMonths(new Date(), 1) });
+                        //   setCookie('user_uni_id', uni_input, { path: '/', expires: addMonths(new Date(), 1) });
+                        //   setCookie('user_stu_id', id_input, { path: '/', expires: addMonths(new Date(), 1) });
+                        //   setCookie('user_password', password_input, { path: '/', expires: addMonths(new Date(), 1) });
+                        //   setCookie('user_type', "user", { path: '/', expires: addMonths(new Date(), 1) });
+                        //   setCookie('user_id', result._id, { path: '/', expires: addMonths(new Date(), 1) });
+                        //   setCookie('user_credit', 0, { path: '/', expires: addMonths(new Date(), 1) });
+                        // });
+                    }
+                  }}>ثبت نام</button> :
+                  <button className='btn btn-outline-light text-center rtl w-100' disabled>
+                    <Spinner animation='border' size='sm' />
+                  </button>}
 
-              <Link className='btn btn-outline-light text-center rtl w-100' to='/login'>ورود</Link>
-            </Form>
+                <Link className='btn btn-outline-light text-center rtl w-100' to='/login'>ورود</Link>
+              </Form>
+            }
           </Card.Body>
         </Card>
       </Container>
